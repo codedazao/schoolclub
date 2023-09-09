b7be891e33038cac03a810059daa3059b02c1dc3
pan <2995332268@qq.com>
2023-09-09 12:03:51 +0800

    初始化项目

---
+++ schoolclubbackend/src/main/java/com/dazao/schoolclubbackend/SchoolClubBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.dazao.schoolclubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolClubBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolClubBackEndApplication.class, args);
+    }
+
+}
